b1b10735f304a932542c4335a2d84f741c50a9a9
Lúcás Meier <cronokirby@gmail.com>
2021-02-22 19:03:13 +0100

    Draw basic rectangle in the canvas

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-alert('hello?')
+const canvas = document.getElementById('root-canvas') as HTMLCanvasElement;
+canvas.width = canvas.clientWidth;
+canvas.height = canvas.clientHeight;
+const ctx = canvas.getContext('2d');
+ctx.beginPath();
+ctx.rect(20, 20, 150, 150);
+ctx.fill();
