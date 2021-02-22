f43806b4b06e95240167b6d74dc0e8659468d763
Lúcás Meier <cronokirby@gmail.com>
2021-02-22 19:28:13 +0100

    Add auxiliary functions for shader and programs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,53 @@
+/**
+ * Create a new shader.
+ *
+ * @param gl the WebGL context
+ * @param type an enumeration representing the kind of shader
+ * @param source the source code for that shader
+ */
+function createShader(
+  gl: WebGLRenderingContext,
+  type: number,
+  source: string,
+): WebGLShader {
+  const shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (!success) {
+    const info = gl.getShaderInfoLog(shader);
+    const message = `Error creating shader: ${info}`;
+    gl.deleteShader(shader);
+    throw new Error(message);
+  }
+  return shader;
+}
+
+/**
+ * Create a new program for rendering.
+ *
+ * @param gl the WebGL Context
+ * @param shaders the shaders to use in the program
+ */
+function createProgram(
+  gl: WebGLRenderingContext,
+  ...shaders: WebGLShader[]
+): WebGLProgram {
+  const program = gl.createProgram();
+  for (const shader of shaders) {
+    gl.attachShader(program, shader);
+  }
+  gl.linkProgram(program);
+  const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (!success) {
+    const info = gl.getProgramInfoLog(program);
+    const message = `Error creating program: ${info}`;
+    gl.deleteProgram(program);
+    throw new Error(message);
+  }
+  return program;
+}
+
 const canvas = document.getElementById('root-canvas') as HTMLCanvasElement;
 canvas.width = canvas.clientWidth;
 canvas.height = canvas.clientHeight;
