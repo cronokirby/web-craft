0da29b413224102df8c72990048cc4926d55464c
Lúcás Meier <cronokirby@gmail.com>
2021-02-23 09:43:50 +0100

    Add a bit of logic to animate color

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,42 @@
 import Renderer from './Renderer';
 
+class Loop {
+  private last: number | null = null;
+  private seconds: number = 0.0;
+
+  constructor(private renderer: Renderer) {}
+
+  private delta(timestamp: number): number {
+    if (this.last === null) {
+      this.last = timestamp;
+    }
+    const delta = timestamp - this.last;
+    this.last = timestamp;
+
+    return delta / 1000;
+  }
+
+  step(timestamp: number) {
+    const delta = this.delta(timestamp);
+    this.seconds += delta;
+
+    this.renderer.draw([
+      0.8,
+      Math.cos(this.seconds),
+      Math.sin(this.seconds),
+      1.0,
+    ]);
+
+    window.requestAnimationFrame((timestamp) => this.step(timestamp));
+  }
+}
+
 const canvas = document.getElementById('root-canvas') as HTMLCanvasElement;
 canvas.width = canvas.clientWidth;
 canvas.height = canvas.clientHeight;
 const gl = canvas.getContext('webgl');
 const renderer = Renderer.init(gl);
 renderer.draw([0.8, 0.3, 0.4, 1.0]);
+
+const loop = new Loop(renderer);
+window.requestAnimationFrame((timestamp) => loop.step(timestamp));
