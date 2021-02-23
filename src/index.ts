c102f7f13afced9fb2ed43c1f6a0fc18863e4c4f
Lúcás Meier <cronokirby@gmail.com>
2021-02-23 18:55:19 +0100

    Add basic controls for moving camera around

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,21 @@ import Renderer from './Renderer';
 import { Color, Seconds, Vec3 } from './math';
 import { Camera } from './Camera';
 
+class ControlState {
+  public left: boolean = false;
+  public right: boolean = false;
+  public forward: boolean = false;
+  public back: boolean = false;
+  public up: boolean = false;
+  public down: boolean = false;
+}
+
 class Loop {
   private last: number | null = null;
   private seconds: number = 0.0;
+  private position: Vec3 = new Vec3(0.0, 0.0, -1.0);
 
-  constructor(private renderer: Renderer) {}
+  constructor(private renderer: Renderer, private controls: ControlState) {}
 
   private delta(timestamp: number): Seconds {
     if (this.last === null) {
@@ -22,19 +32,96 @@ class Loop {
     const delta = this.delta(timestamp);
     this.seconds += delta;
 
+    if (this.controls.left) {
+      this.position.x -= delta;
+    }
+    if (this.controls.right) {
+      this.position.x += delta;
+    }
+    if (this.controls.forward) {
+      this.position.z += delta;
+    }
+    if (this.controls.back) {
+      this.position.z -= delta;
+    }
+    if (this.controls.up) {
+      this.position.y += delta;
+    }
+    if (this.controls.down) {
+      this.position.y -= delta;
+    }
+
     const ar = this.renderer.calculateAR();
     this.renderer.draw(
-      new Camera(new Vec3(0.0, 0.0, -1.0), 0.0, 0.0, ar),
+      new Camera(this.position, 0.0, 0.0, ar),
       new Color(0.8, Math.cos(this.seconds), Math.sin(this.seconds)),
-      this.seconds * 45
+      this.seconds * 45,
     );
-
     window.requestAnimationFrame((timestamp) => this.step(timestamp));
   }
 }
 
 const canvas = document.getElementById('root-canvas') as HTMLCanvasElement;
+
+const controls = new ControlState();
+window.addEventListener('keydown', (event) => {
+  switch (event.key) {
+    case 'd': {
+      controls.right = true;
+      break;
+    }
+    case 'a': {
+      controls.left = true;
+      break;
+    }
+    case 'w': {
+      controls.forward = true;
+      break;
+    }
+    case 's': {
+      controls.back = true;
+      break;
+    }
+    case 'Shift': {
+      controls.down = true;
+      break;
+    }
+    case ' ': {
+      controls.up = true;
+      break;
+    }
+  }
+});
+window.addEventListener('keyup', (event) => {
+  switch (event.key) {
+    case 'd': {
+      controls.right = false;
+      break;
+    }
+    case 'a': {
+      controls.left = false;
+      break;
+    }
+    case 'w': {
+      controls.forward = false;
+      break;
+    }
+    case 's': {
+      controls.back = false;
+      break;
+    }
+    case 'Shift': {
+      controls.down = false;
+      break;
+    }
+    case ' ': {
+      controls.up = false;
+      break;
+    }
+  }
+});
+
 const gl = canvas.getContext('webgl');
 const renderer = Renderer.init(gl);
-const loop = new Loop(renderer);
+const loop = new Loop(renderer, controls);
 window.requestAnimationFrame((timestamp) => loop.step(timestamp));
