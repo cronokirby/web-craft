2219db2cc181656db6d1b107fc6423852f18f156
Lúcás Meier <cronokirby@gmail.com>
2021-02-24 18:07:51 +0100

    Add spinning cube

--- src/Renderer.ts
+++ src/Renderer.ts
@@ -110,12 +110,15 @@ export default class Renderer {
   }
 
   draw(camera: Camera, color: Color, angle: AngleDeg) {
+    this.gl.enable(this.gl.DEPTH_TEST);
     this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
     this.gl.clearColor(0, 0, 0, 1);
     this.gl.clear(this.gl.COLOR_BUFFER_BIT);
     this.gl.useProgram(this.program);
 
     let mat = Mat4.identity();
+    mat = Mat4.translation(-0.5, -0.5, -0.5).mul(mat);
+    mat = Mat4.rotZ(angle / 2).mul(mat);
     mat = Mat4.rotY(angle).mul(mat);
     mat = camera.viewProjection().mul(mat);
     this.gl.uniformMatrix4fv(this.uniforms.u_view, false, mat.columns());
@@ -124,15 +127,45 @@ export default class Renderer {
     this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.buffers.position);
     this.gl.vertexAttribPointer(
       this.attributes.a_position,
-      2,
+      3,
       this.gl.FLOAT,
       false,
       0,
       0,
     );
+    const vertBuf = new Float32Array(6 * 6 * 3);
+    {
+      let i = 0;
+      const face = (a, b, c, d) => {
+        vertBuf[i++] = a[0];
+        vertBuf[i++] = a[1];
+        vertBuf[i++] = a[2];
+        vertBuf[i++] = b[0];
+        vertBuf[i++] = b[1];
+        vertBuf[i++] = b[2];
+        vertBuf[i++] = c[0];
+        vertBuf[i++] = c[1];
+        vertBuf[i++] = c[2];
+        vertBuf[i++] = b[0];
+        vertBuf[i++] = b[1];
+        vertBuf[i++] = b[2];
+        vertBuf[i++] = c[0];
+        vertBuf[i++] = c[1];
+        vertBuf[i++] = c[2];
+        vertBuf[i++] = d[0];
+        vertBuf[i++] = d[1];
+        vertBuf[i++] = d[2];
+      }
+      face([0, 0, 0], [0, 1, 0], [1, 0, 0], [1, 1, 0]);
+      face([0, 0, 1], [0, 1, 1], [1, 0, 1], [1, 1, 1]);
+      face([0, 0, 0], [0, 1, 0], [0, 0, 1], [0, 1, 1]);
+      face([1, 0, 0], [1, 1, 0], [1, 0, 1], [1, 1, 1]);
+      face([0, 0, 0], [0, 0, 1], [1, 0, 0], [1, 0, 1]);
+      face([0, 1, 0], [0, 1, 1], [1, 1, 0], [1, 1, 1]);
+    }
     this.gl.bufferData(
       this.gl.ARRAY_BUFFER,
-      new Float32Array([0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1]),
+      vertBuf,
       this.gl.STATIC_DRAW,
     );
 
@@ -148,12 +181,29 @@ export default class Renderer {
     );
     const c1 = [58, 228, 246];
     const c2 = [200, 58, 246];
+    const c3 = [211, 246, 58];
+    const c4 = [67, 246, 58];
+    const c5 = [246, 58, 83];
+    const c6 = [58, 74, 246];
+    const colorBuf = new Uint8Array(6 * 6 * 3);
+
+    {
+      let i = 0;
+      for (const color of [c1, c2, c3, c4, c5, c6]) {
+        for (let j = 0; j < 6; ++j) {
+          colorBuf[i++] = color[0];
+          colorBuf[i++] = color[1];
+          colorBuf[i++] = color[2];
+        }
+      }
+    }
+
     this.gl.bufferData(
       this.gl.ARRAY_BUFFER,
-      new Uint8Array([...c1, ...c1, ...c1, ...c2, ...c2, ...c2]),
+      colorBuf,
       this.gl.STATIC_DRAW,
     );
 
-    this.gl.drawArrays(this.gl.TRIANGLES, 0, 6);
+    this.gl.drawArrays(this.gl.TRIANGLES, 0, 6 * 6);
   }
 }
--- src/shaders/vert.ts
+++ src/shaders/vert.ts
@@ -1,5 +1,5 @@
 const vert = `
-attribute vec2 a_position;
+attribute vec3 a_position;
 attribute vec3 a_color;
 
 uniform mat4 u_view;
@@ -7,7 +7,7 @@ uniform mat4 u_view;
 varying vec4 v_color;
 
 void main() {
-  gl_Position = u_view * vec4(a_position, 0.0, 1.0);
+  gl_Position = u_view * vec4(a_position, 1.0);
 
   v_color = vec4(a_color, 1.0);
 }
